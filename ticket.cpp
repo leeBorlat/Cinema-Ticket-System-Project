a53c17fec2da50ec5a762e018f3d3ce3a5985dfc
leeBorlat <ryanborlat42@gmail.com>
2021-01-15 19:37:21 +0800

    master

--- ticket.cpp
+++ ticket.cpp
@@ -4,7 +4,13 @@
 using namespace std;
 void seats(void);
 void page(void);
+void message(void);
 char space();
+//PANG INITIALIZE NG MGA FUNCTIONS
+
+char rowNo;
+int colNo;
+//UNIVERSAL VARIABLES
 
 char space()
 {
@@ -24,6 +30,17 @@ void page(void)
     cout << space() << "**************************" << endl;
 }
 
+void message()
+{
+
+    cout << space() << "Where would you like to reserve your seat? \n"<< endl;
+    cout << space() << "Row Letter: ";
+    cin >> rowNo;
+    rowNo = toupper(rowNo);
+    cout << space() << "Column Number: ";
+    cin >> colNo;
+    return;
+}
 int main()
 //start of code
 
@@ -34,13 +51,14 @@ int main()
     cout << endl;
     cout << endl;
 
-    cout << space() << "Seats available: \n"<< endl;
-char table[6][6] = {{' ', '1', '2', '3', '4', '5'},
-                                {'A', 'X', '.', '.', '.', '.'},
-                                {'B', '.', '.', '.', '.', '.'},
-                                {'C', '.', '.', '.', '.', '.'},
-                                {'D', '.', '.', '.', '.', '.'},
-                                {'E', '.', '.', '.', '.', '.'}};
+    cout << space() << "Seats available: \n"
+         << endl;
+    char table[6][6] = {{' ', '1', '2', '3', '4', '5'},
+                        {'A', '.', '.', '.', '.', '.'},
+                        {'B', '.', '.', '.', '.', '.'},
+                        {'C', '.', '.', '.', '.', '.'},
+                        {'D', '.', '.', '.', '.', '.'},
+                        {'E', '.', '.', '.', '.', '.'}};
     for (int x = 0; x <= 5; x++)
     {
         cout << space();
@@ -48,46 +66,37 @@ char table[6][6] = {{' ', '1', '2', '3', '4', '5'},
         for (int y = 0; y <= 5; y++)
         {
             cout << table[x][y] << "  ";
-
         }
 
         cout << "\n\n";
     }
-    char rowNo;
-    int colNo;
 
-    cout << space() << "Where would you like to reserve your seat? \n"<< endl;
-    cout << space() << "Row Letter: ";
-    cin >> rowNo;
-    cout << space() << "Column Number: ";
-    cin >> colNo;
+    message();
 
     int transRowNo;
-    switch(rowNo)
+    switch (rowNo)
     {
-        case 'A':
-        transRowNo=1; 
-        break; 
-        case 'B':
-        transRowNo=2;
+    case 'A':
+        transRowNo = 1;
         break;
-        case 'C':
-        transRowNo=3;
+    case 'B':
+        transRowNo = 2;
         break;
-        case 'D':
-        transRowNo=4;
+    case 'C':
+        transRowNo = 3;
         break;
-        case 'E':
-        transRowNo=5;
+    case 'D':
+        transRowNo = 4;
         break;
+    case 'E':
+        transRowNo = 5;
+        break;
+    }
 
-    } 
-    cout << table[transRowNo][colNo];
-
-if(table[transRowNo][colNo]=='.')
-{
-    table[transRowNo][colNo]='+';
-}
+    if (table[transRowNo][colNo] == '.')
+    {
+        table[transRowNo][colNo] = '+';
+    }
 
     for (int x = 0; x <= 5; x++)
     {
@@ -96,10 +105,19 @@ if(table[transRowNo][colNo]=='.')
         for (int y = 0; y <= 5; y++)
         {
             cout << table[x][y] << "  ";
-
         }
 
         cout << "\n\n";
     }
+
     return 0;
 }
+
+
+//things to do 
+// i loop yung program
+// make a main menu
+// dapat ma save na yung values sa database tapos mukhang ticket na malupet
+// dapat ma check kung reserved or hindi pa
+// dapat maka group reserve
+//TAPOS TAPOS NA?
