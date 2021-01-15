8ef3f6bed644b374ea512874402af5011d9c28fd
leeBorlat <ryanborlat42@gmail.com>
2021-01-15 19:02:23 +0800

    Save para di masira putangina

--- ticket.cpp
+++ ticket.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
-void page(void);
 void seats(void);
+void page(void);
 char space();
 
 char space()
@@ -22,29 +23,9 @@ void page(void)
     cout << space() << "*                        *" << endl;
     cout << space() << "**************************" << endl;
 }
-void seats(void)
-{
-    for (int x = 0; x <= 5; x++)
-    {
-        cout << space();
 
-        for (int y = 0; y <= 5; y++)
-        {
-            
-            char table[6][6] = {{' ', '1', '2', '3', '4', '5'},
-                                {'A', '.', '.', '.', '.', '.'},
-                                {'B', '.', '.', '.', '.', '.'},
-                                {'C', '.', '.', '.', '.', '.'},
-                                {'D', '.', '.', '.', '.', '.'},
-                                {'E', '.', '.', '.', '.', '.'}};
-
-            cout << table[x][y] << "  ";
-        }
-
-        cout << "\n\n";
-    }
-}
 int main()
+//start of code
 
 {
     page();
@@ -54,16 +35,71 @@ int main()
     cout << endl;
 
     cout << space() << "Seats available: \n"<< endl;
+char table[6][6] = {{' ', '1', '2', '3', '4', '5'},
+                                {'A', 'X', '.', '.', '.', '.'},
+                                {'B', '.', '.', '.', '.', '.'},
+                                {'C', '.', '.', '.', '.', '.'},
+                                {'D', '.', '.', '.', '.', '.'},
+                                {'E', '.', '.', '.', '.', '.'}};
+    for (int x = 0; x <= 5; x++)
+    {
+        cout << space();
 
-    seats();
+        for (int y = 0; y <= 5; y++)
+        {
+            cout << table[x][y] << "  ";
+
+        }
+
+        cout << "\n\n";
+    }
     char rowNo;
     int colNo;
 
-    cout << space() << "Where would you like to reserve your seat? \n" << endl;
-    cout << "Row Letter: ";
+    cout << space() << "Where would you like to reserve your seat? \n"<< endl;
+    cout << space() << "Row Letter: ";
     cin >> rowNo;
-    cout <<"Column Number: ";
+    cout << space() << "Column Number: ";
     cin >> colNo;
 
+    int transRowNo;
+    switch(rowNo)
+    {
+        case 'A':
+        transRowNo=1; 
+        break; 
+        case 'B':
+        transRowNo=2;
+        break;
+        case 'C':
+        transRowNo=3;
+        break;
+        case 'D':
+        transRowNo=4;
+        break;
+        case 'E':
+        transRowNo=5;
+        break;
+
+    } 
+    cout << table[transRowNo][colNo];
+
+if(table[transRowNo][colNo]=='.')
+{
+    table[transRowNo][colNo]='+';
+}
+
+    for (int x = 0; x <= 5; x++)
+    {
+        cout << space();
+
+        for (int y = 0; y <= 5; y++)
+        {
+            cout << table[x][y] << "  ";
+
+        }
+
+        cout << "\n\n";
+    }
     return 0;
 }
